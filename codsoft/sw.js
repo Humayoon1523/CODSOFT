e72d61df075ae6b69ab3961d293eef55c4461e0f
Humayoon1523 <mohamedhumayoon2007@gmail.com>
2026-03-04 22:30:10 +0530

    Add files via upload

---
+++ codsoft/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'calculator-pwa-v1';
+const urlsToCache = [
+    './calculater.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
